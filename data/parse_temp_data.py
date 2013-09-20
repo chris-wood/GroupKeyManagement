4fb5151f37f41cd36f61aa2374d80b719b0e3276
caw <christopherwood07@gmail.com>
2013-09-20 08:50:48 -0700

    Adding output data...

--- data/parse_temp_data.py
+++ data/parse_temp_data.py
@@ -2,26 +2,36 @@ import sys
 
 # params
 fname = sys.argv[1]
-k = sys.argv[2]
-m = sys.argv[3]
-p1 = sys.argv[4]
-p2 = sys.argv[5]
 
 # parsing
-times = []
+times = {}
 fn = open(fname, 'r')
 for line in fn:
 	line = line.strip()
-	if "," in line:
+	if "," in line and "=" not in line: # skip debug lines
 		data = line.split(",")
 		for i in range(len(data)):
 			data[i] = data[i].strip()
-		if data[0] == k and data[1] == m and data[3] == p1 and data[4] == p2:
-			print >> sys.stderr, line
-			times.append(data[5])
+
+		# Retrieve the parameters for this simulation
+		print(data)
+		k = int(data[0])
+		m = int(data[1])
+		n = int(data[2])
+		p1 = float(data[3])
+		p2 = float(data[4])
+		time = float(data[5])
+		stddev = float(data[6])
+		stderr = float(data[7])
+
+		# Group by k,m,p1,p2 (keyed tuple) and store tuple (n, time, stddev, stderr)
+		if not ((k,m,p1,p2) in times):
+			times[(k,m,p1,p2)] = []
+		times[(k,m,p1,p2)].append((n, time, stddev, stderr))
 
 # print times
-print "k m p1 p2"
-print str(k) + " " + str(m) + " " + str(p1) + " " + str(p2)
-for t in times:
-	print(t)
+for tup in times:
+	fname = "data_" + str(tup[0]) + "_" + str(tup[1]) + "_" + str(tup[2]) + "_" + str(tup[3])
+	fout = open(fname, 'w')
+	for data in times[tup]:
+		fout.write(str(data[0]) + "," + str(data[1]) + "," + str(data[2]) + "," + str(data[3]) + "\n")
